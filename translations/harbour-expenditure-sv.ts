e6200921aa925e0a4fd6981216783bde7a8e4864
Åke Engelbrektson <eson57@users.noreply.github.com>
2023-12-02 11:58:33 +0100

    Create harbour-expenditure-sv.ts
    Add Swedish translation

---
+++ translations/harbour-expenditure-sv.ts
@@ -0,0 +1,442 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>Expenditure</source>
+        <translation>Expenditure</translation>
+    </message>
+    <message>
+        <source>License: GPL v3</source>
+        <translation>Licens: GPL v3</translation>
+    </message>
+    <message>
+        <source>Expenditure is a tool to track and split bills, project or trip expenses in multiple currencies among groups.</source>
+        <translation>Expenditure är ett verktyg för att hålla koll på och dela upp räkningar, projekt- eller reseutgifter i flera valutor, mellan grupper.</translation>
+    </message>
+    <message>
+        <source>Thanksgiving, feedback and support is always welcome.</source>
+        <translation>Tacksamhet, återkoppling och stöd är alltid välkommet.</translation>
+    </message>
+    <message>
+        <source>In case of any database error tap 10x on the word &apos;Settings&apos; for cleanup options.</source>
+        <translation>Snabbtryck 10 gånger på rubriken &quot;Inställningar&quot;, för rensningsalternativ vid databasfel.</translation>
+    </message>
+    <message>
+        <source>First author:</source>
+        <translation>Ursprunglig utveckalare:</translation>
+    </message>
+    <message>
+        <source>Tobias Planitzer</source>
+        <translation>Tobias Planitzer</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>Troubleshooting</source>
+        <translation>Felhantering</translation>
+    </message>
+    <message>
+        <source>Source code</source>
+        <translation>Källkod</translation>
+    </message>
+</context>
+<context>
+    <name>BannerAddExpense</name>
+    <message>
+        <source>expense</source>
+        <translation>Utgiftspost</translation>
+    </message>
+    <message>
+        <source>info</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <source>payment by</source>
+        <translation>Betald av</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Spara</translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation>Lägg till</translation>
+    </message>
+    <message>
+        <source>price</source>
+        <translation>Pris</translation>
+    </message>
+    <message>
+        <source>currency</source>
+        <translation>Valuta</translation>
+    </message>
+    <message>
+        <source>beneficiary</source>
+        <translation>Förmånstagare</translation>
+    </message>
+</context>
+<context>
+    <name>BannerAddProject</name>
+    <message>
+        <source>Add</source>
+        <translation>Lägg till</translation>
+    </message>
+    <message>
+        <source>Project</source>
+        <translation>Projekt</translation>
+    </message>
+    <message>
+        <source>name</source>
+        <translation>Namn</translation>
+    </message>
+    <message>
+        <source>base currency</source>
+        <translation>Basvaluta</translation>
+    </message>
+    <message>
+        <source>Members</source>
+        <translation>Medlemmar</translation>
+    </message>
+    <message>
+        <source>rename</source>
+        <translation>Byt namn</translation>
+    </message>
+    <message>
+        <source>remove</source>
+        <translation>Ta bort</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Spara</translation>
+    </message>
+    <message>
+        <source>Delete this project?</source>
+        <translation>Vill du ta bort det här projektet?</translation>
+    </message>
+    <message>
+        <source>Clear all transactions?</source>
+        <translation>Vill du ta bort alla transaktioner?</translation>
+    </message>
+    <message>
+        <source>create</source>
+        <translation>Skapa</translation>
+    </message>
+    <message>
+        <source>edit</source>
+        <translation>Redigera</translation>
+    </message>
+    <message>
+        <source>Backup</source>
+        <translation>Säkerhetskopiera</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Ta bort</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation>Nollställ</translation>
+    </message>
+    <message>
+        <source>Restore</source>
+        <translation>Återställ</translation>
+    </message>
+</context>
+<context>
+    <name>CalcPage</name>
+    <message>
+        <source>payments</source>
+        <translation>Betalningar</translation>
+    </message>
+    <message>
+        <source>saldo</source>
+        <translation>Saldo</translation>
+    </message>
+    <message>
+        <source>owes</source>
+        <translation>är skyldig</translation>
+    </message>
+    <message>
+        <source>EXCHANGE RATES</source>
+        <translation>VÄXELKURSER</translation>
+    </message>
+    <message>
+        <source>SPENDING OVERVIEW</source>
+        <translation>UTGIFTSÖVERSIKT</translation>
+    </message>
+    <message>
+        <source>SETTLEMENT SUGGESTION</source>
+        <translation>LÖSNINGSFÖRSLAG</translation>
+    </message>
+    <message>
+        <source>the sum of</source>
+        <translation>summan av</translation>
+    </message>
+    <message>
+        <source>Project:</source>
+        <translation>Projekt:</translation>
+    </message>
+    <message>
+        <source>Total expenses</source>
+        <translation>Totala kostnader</translation>
+    </message>
+    <message>
+        <source>payed</source>
+        <translation>Betalade</translation>
+    </message>
+    <message>
+        <source>received</source>
+        <translation>Tog emot</translation>
+    </message>
+    <message>
+        <source>Settlement suggestion:</source>
+        <translation>Lösningsförslag:</translation>
+    </message>
+    <message>
+        <source>item:</source>
+        <translation>Objekt:</translation>
+    </message>
+    <message>
+        <source>payer:</source>
+        <translation>Betalare:</translation>
+    </message>
+    <message>
+        <source>price:</source>
+        <translation>Pris:</translation>
+    </message>
+    <message>
+        <source>info:</source>
+        <translation>Info:</translation>
+    </message>
+    <message>
+        <source>beneficiaries:</source>
+        <translation>Mottagare:</translation>
+    </message>
+    <message>
+        <source>Detailed Spendings:</source>
+        <translation>Detaljerade utgifter:</translation>
+    </message>
+    <message>
+        <source>Expense #</source>
+        <translation>Utgift #</translation>
+    </message>
+    <message>
+        <source>date:</source>
+        <translation>Datum/tid:</translation>
+    </message>
+    <message>
+        <source>Share detailed</source>
+        <translation>Dela detaljerat</translation>
+    </message>
+    <message>
+        <source>Share compact</source>
+        <translation>Dela kompakt</translation>
+    </message>
+    <message>
+        <source>name</source>
+        <translation>Namn</translation>
+    </message>
+    <message>
+        <source>benefits</source>
+        <translation>Förmåner</translation>
+    </message>
+    <message>
+        <source>Results</source>
+        <translation>Resultat</translation>
+    </message>
+    <message>
+        <source>constant</source>
+        <translation>Konstant</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>Create new project.</source>
+        <translation>Skapa nytt projekt.</translation>
+    </message>
+    <message>
+        <source>Nothing loaded yet.</source>
+        <translation>Inget inläst ännu.</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Redigera</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Ta bort</translation>
+    </message>
+    <message>
+        <source>Calculate</source>
+        <translation>Beräkna</translation>
+    </message>
+    <message>
+        <source>group:</source>
+        <translation>Grupp:</translation>
+    </message>
+    <message>
+        <source>Expenses</source>
+        <translation>Utgifter</translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation>Lägg till</translation>
+    </message>
+    <message>
+        <source>Remove entry?</source>
+        <translation>Vill du ta bort posten?</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>Project</source>
+        <translation>Projekt</translation>
+    </message>
+    <message>
+        <source>Sorting</source>
+        <translation>Sortering</translation>
+    </message>
+    <message>
+        <source>descending</source>
+        <translation>Fallande</translation>
+    </message>
+    <message>
+        <source>ascending</source>
+        <translation>Stigande</translation>
+    </message>
+    <message>
+        <source>Exchange rate</source>
+        <translation>Valutakurs</translation>
+    </message>
+    <message>
+        <source>Delete stored exchange rates?</source>
+        <translation>Vill du ta bort sparade valutakurser?</translation>
+    </message>
+    <message>
+        <source>Delete stored projects?</source>
+        <translation>Vill du ta bort sparade projekt?</translation>
+    </message>
+    <message>
+        <source>Delete stored settings?</source>
+        <translation>Vill du ta bort sparade inställningar?</translation>
+    </message>
+    <message>
+        <source>per currency (constant)</source>
+        <translation>per valuta (konstant)</translation>
+    </message>
+    <message>
+        <source>per transaction (dates)</source>
+        <translation>per transaktion (datum)</translation>
+    </message>
+    <message>
+        <source>Restore backup file</source>
+        <translation>Återställ säkerhetskopia</translation>
+    </message>
+    <message>
+        <source>Backup to</source>
+        <translation>Säkerhetskopiera till</translation>
+    </message>
+    <message>
+        <source>Scrollbar</source>
+        <translation>Rullningslist</translation>
+    </message>
+    <message>
+        <source>interactive (beta)</source>
+        <translation>Interaktiv (beta)</translation>
+    </message>
+    <message>
+        <source>normal</source>
+        <translation>Normal</translation>
+    </message>
+    <message>
+        <source>Backup successful</source>
+        <translation>Säkherhetskopiering slutförd</translation>
+    </message>
+    <message>
+        <source>Replace</source>
+        <translation>Ersätt</translation>
+    </message>
+    <message>
+        <source>Merge</source>
+        <translation>Slå samman</translation>
+    </message>
+    <message>
+        <source>File saved to:</source>
+        <translation>Fil sparad till:</translation>
+    </message>
+    <message>
+        <source>Validity check failed.</source>
+        <translation>Validitetskontroll misslyckades.</translation>
+    </message>
+    <message>
+        <source>This backup file does not seem to be created by Expenditure:</source>
+        <translation>Den här säkerhetskopian verkar inte ha skapats av Expenditure:</translation>
+    </message>
+    <message>
+        <source>Backup successfully restored.</source>
+        <translation>Säkerhetskopia återställd.</translation>
+    </message>
+    <message>
+        <source>Project expenses have been overwritten by backup-file expenses.</source>
+        <translation>Projektkostnader har skrivits över av utgifter från säkerhetskopia.</translation>
+    </message>
+    <message>
+        <source>Project expenses have been merged with backup-file expenses.</source>
+        <translation>Projektkostnader har slagits samman med utgifter från säkerhetskopia.</translation>
+    </message>
+    <message>
+        <source>Restore backup - choose action</source>
+        <translation>Återställ säkerhetskopia - Välj åtgärd</translation>
+    </message>
+    <message>
+        <source>Replace deletes all former project-expenses and uses those from backup-file instead.</source>
+        <translation>Ersätt, tar bort alla tidigare projektkostnader och använder dem från säkerhetskopian istället.</translation>
+    </message>
+    <message>
+        <source>Merge keeps former project-expenses and adds those from backup-file which are not yet on the list.</source>
+        <translation>Slå samman, behåller tidigare projektutgifter och lägger till de från säkerhetskopian som ännu inte finns på listan.</translation>
+    </message>
+    <message>
+        <source>File info: This backup was created by a different project.</source>
+        <translation>Filinformation: Den här säkerhetskopian skapades av ett annat projekt.</translation>
+    </message>
+    <message>
+        <source>File info: This backup was created by the original project.</source>
+        <translation>Filinformation: Den här säkerhetskopian skapades av det ursprungliga projektet.</translation>
+    </message>
+    <message>
+        <source>settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>exchange rates</source>
+        <translation>Valutakurser</translation>
+    </message>
+    <message>
+        <source>projects</source>
+        <translation>Projekt</translation>
+    </message>
+    <message>
+        <source>Database cleanup - requires restart:</source>
+        <translation>Databasrensning – Kräver omstart:</translation>
+    </message>
+</context>
+</TS>
